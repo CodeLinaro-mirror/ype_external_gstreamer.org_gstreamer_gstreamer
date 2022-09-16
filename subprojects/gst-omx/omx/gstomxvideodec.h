20bf7ac2c72311811900a93f6b9e9d12c2842d63
Lily Li <quic_lali@quicinc.com>
2022-09-16 16:36:29 +0800

    gst-omx: dec: add output picture order and low latency mode
    add log when gst_omx_component_set_parameter() for those mode
    
    original patch: 3533884
    
    Change-Id: I0f24ee3c39030e8665c3b6ed6406b3c32e72b42d
    Signed-off-by: Lily Li <quic_lali@quicinc.com>

--- subprojects/gst-omx/omx/gstomxvideodec.h
+++ subprojects/gst-omx/omx/gstomxvideodec.h
@@ -115,6 +115,8 @@ struct _GstOMXVideoDec
   gint width;
   gint height;
   gboolean video_info_changed;
+  guint output_picture_order_mode;
+  gboolean low_latency_mode;
 #ifdef USE_GBM
   void* gbm_lib;
   struct gbm_device * (*gbm_api_create_device)(int fd);
