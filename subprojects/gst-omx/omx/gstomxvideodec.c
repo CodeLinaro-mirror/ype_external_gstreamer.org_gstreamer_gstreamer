bdd71e7ca773b609c5f1e4ad8c4a9b12ddfd88cb
Lily Li <quic_lali@quicinc.com>
2022-10-21 15:50:51 +0800

    gst-omx: dec: fix for sps inline encoded file can't play
    revert:
    https://github.com/GStreamer/gst-omx/commit/006e0636fd09068fb39b66ba5f44234b19570258
    https://github.com/GStreamer/gst-omx/commit/503db0f1af09cbccb43007a123a219f9b8c68c23
    for sps inline encoded file play failure issue.
    
    Change-Id: I684344eaf83fd9f80384ca9c9197b90d525e48f2
    Signed-off-by: Lily Li <quic_lali@quicinc.com>

--- subprojects/gst-omx/omx/gstomxvideodec.c
+++ subprojects/gst-omx/omx/gstomxvideodec.c
@@ -3527,7 +3527,7 @@ gst_omx_video_dec_handle_frame (GstVideoDecoder * decoder,
   }
 
   if (!self->started) {
-    if (!GST_VIDEO_CODEC_FRAME_IS_SYNC_POINT (frame) && !header) {
+    if (!GST_VIDEO_CODEC_FRAME_IS_SYNC_POINT (frame)) {
       gst_video_decoder_drop_frame (GST_VIDEO_DECODER (self), frame);
       return GST_FLOW_OK;
     }
@@ -3658,6 +3658,7 @@ gst_omx_video_dec_handle_frame (GstVideoDecoder * decoder,
       }
 
       buf->omx_buf->nFlags |= OMX_BUFFERFLAG_CODECCONFIG;
+      buf->omx_buf->nFlags |= OMX_BUFFERFLAG_ENDOFFRAME;
 
       if (GST_CLOCK_TIME_IS_VALID (timestamp))
         GST_OMX_SET_TICKS (buf->omx_buf->nTimeStamp,
@@ -3741,9 +3742,6 @@ gst_omx_video_dec_handle_frame (GstVideoDecoder * decoder,
 
     if (first_ouput_buffer && GST_VIDEO_CODEC_FRAME_IS_SYNC_POINT (frame))
       buf->omx_buf->nFlags |= OMX_BUFFERFLAG_SYNCFRAME;
-
-    if (header)
-      buf->omx_buf->nFlags |= OMX_BUFFERFLAG_CODECCONFIG;
 
     /* TODO: Set flags
      *   - OMX_BUFFERFLAG_DECODEONLY for buffers that are outside
